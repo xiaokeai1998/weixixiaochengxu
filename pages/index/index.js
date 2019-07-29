dd533730126923e94240aac84e6b5e8fcf981e69
unknown <wumenx>
2019-07-29 23:00:37 +0800

    轮播图的完成

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,66 +1,25 @@
-// pages/index/index.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
+    // 轮播图数据源
+    SwiperList: [],
+  },
+  // 页面开始的时候加载
+  onLoad () {
+    this.getSwiperList()
 
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  // 轮播图页面数据接口
+  getSwiperList(e){
+    wx.request({
+      url: 'https://api.zbztb.cn/api/public/v1/home/swiperdata',
+      success: (result) => {
+          console.log(result)
+        this.setData({
+          SwiperList: result.data.message
+        })
+      },
+      
+    })
+   
   }
 })
