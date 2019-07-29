bcecee688187be44d0a0a1f795dd5d940b306eb5
unknown <wumenx>
2019-07-29 23:40:32 +0800

    分类导航栏的完成

--- pages/index/index.js
+++ pages/index/index.js
@@ -2,18 +2,20 @@ Page({
   data: {
     // 轮播图数据源
     SwiperList: [],
+    // 分类导航数组
+    NavrCategory:[]
   },
   // 页面开始的时候加载
   onLoad () {
-    this.getSwiperList()
-
+    this.getSwiperList(),
+    this.getNavrCategory()
   },
   // 轮播图页面数据接口
   getSwiperList(e){
     wx.request({
       url: 'https://api.zbztb.cn/api/public/v1/home/swiperdata',
       success: (result) => {
-          console.log(result)
+          // console.log(result)
         this.setData({
           SwiperList: result.data.message
         })
@@ -21,5 +23,20 @@ Page({
       
     })
    
+  },
+
+  // 获取导航数据
+  getNavrCategory(){
+    wx.request({
+      url: 'https://api.zbztb.cn/api/public/v1/home/catitems',
+      success: (result) => {
+          console.log(result)
+          this.setData({
+            NavrCategory: result.data.message
+          })
+      },
+
+    })
   }
+
 })
