6a91b228bf4162a32d9ef25899226d08b9dc2767
unknown <wumenx>
2019-07-31 11:22:33 +0800

    分类功能的完成

--- pages/category/category.js
+++ pages/category/category.js
@@ -4,26 +4,46 @@ Page({
     // 左边列表数据
     CategoryLeftList:[],
     // 右侧列表数据
-    CategoryRightList:[]
+    CategoryRightList:[],
+    // 表示菜单的索引
+    currentIndex: 0,
+    // 滚动条
+    scrollTap: 0
   },
 //  页面开始的是加载
 onLoad () {
     // h获取页面接口的数据
     this.getCategoryList()
 },
+// 获取接口返回的数据，数组形式
+Cates:[],
 // 获取左边分类的数据
 getCategoryList(){
     request({url:"/categories"})
     .then(result =>{
+      this.Cates = result;
       
       // 左侧菜单要的数据
-    const CategoryLeftList = result.map(v => ({cat_id:v.cat_id,cat_name:v.cat_name}));
-    const CategoryRightList = result[0].children
-    console.log(result)
+    const CategoryLeftList = this.Cates.map(v => ({cat_id:v.cat_id,cat_name:v.cat_name}));
+    const CategoryRightList = this.Cates[0].children
+    // console.log(result)
       this.setData({
         CategoryLeftList,
         CategoryRightList
       })
     })
+},
+
+// 给左侧添加绑定事件
+handelMessChang(e){
+  console.log(e)
+  // 获取索引
+  const { index } = e.currentTarget.dataset;
+  const CategoryRightList = this.Cates[index].children
+  this.setData({
+    currentIndex:index,
+    CategoryRightList
+  
+  })
 }
 })
