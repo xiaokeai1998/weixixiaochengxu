33080e59b2f387b9060c7fcb7b3c59a20be8e93a
unknown <wumenx>
2019-08-01 22:33:48 +0800

    tabs栏的完成

--- components/tabs/tabs.js
+++ components/tabs/tabs.js
@@ -19,6 +19,12 @@ Component({
    * 组件的方法列表
    */
   methods: {
-
+    handleTapChang(e){
+      console.log(e)
+      const {index}=e.currentTarget.dataset;
+      // 触发父组件的自定义事件
+      this.triggerEvent("itemChange",{index});
+    },
+  
   }
 })
--- pages/goods_list/goods_list.js
+++ pages/goods_list/goods_list.js
@@ -20,9 +20,9 @@ Page({
       pagesize: 10,//页容量
     },
   // 当前页面的加载
-  onLoad(option){
+  onLoad(options){
     // 形参可以获取到url上面的参数
-    this.QueryParams.cid = option.cid
+    this.QueryParams.cid = options.cid
     this.getGoods_list()
   },
 
@@ -30,12 +30,24 @@ Page({
   getGoods_list(){
     request({url:"/goods/search",data:this.QueryParams})
     .then(result =>{
-      console.log(result)
+      // console.log(result)
       this.setData({
         gooodsLIst:result.goods
       })
     })
 
+  },
+    // 子组件触发
+    handleItemChang(e){
+      // console.log(e)
+      // 获取传递过来的索引
+      const {index} = e.detail;
+      // 获取tabs数组
+      const {tabs} = this.data;
+      // 循环修改tabs数组
+      tabs.forEach((v,i) =>i === index?v.isActive=true:v.isActive=false);
+      this.setData({tabs});
   }
+
 
 })
